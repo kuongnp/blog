6cee9c6d5f3f32b418cdef485c9f47476d605781
nguyen phu cuong <cuong.nguyenphu@vti.com.vn>
2020-04-28 16:15:14 +0700

    add structure

---
+++ api/plugins/comment/index.js
@@ -0,0 +1,17 @@
+'use strict';
+import router from './router';
+const category = {
+    name: 'category',
+    version: '1.0.0',
+    register: async function (server, options) {
+        server.route({
+            method: 'GET',
+            path: '/test',
+            handler: function (request, h) {
+                return 'hello, world';
+            }
+        });
+        // etc ...
+        await someAsyncMethods();
+    }
+};
